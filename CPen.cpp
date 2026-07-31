368c92f4c1331de4a3ef1159564271e236b2f13e
None <mihailburcinsij@gmail.com>
2026-07-31 18:17:23 +0300

    Add files via upload

---
+++ CPen.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Misha on 31.07.2026.
+//
+#include "raylib.h"
+#include "Vec2d.h"
+#include "CNaovigator.h"
+class CPen {
+private:
+CNavigator cn;
+Vec2d pos;
+Color cp;
+float  thick;
+public:
+    void NewColor(Color color) {
+        cp = color;
+
+    }
+    CPen() {
+        cp = BLACK;
+    }
+    void NewThickness(float thick_new) {
+        thick = thick_new;
+    }
+    void forward(float fd) {
+        Vec2d r =cn.math_line(pos,fd);
+        DrawLineEx({pos.xcor(),pos.ycor()},{r.xcor(),r.ycor()},thick,cp);
+    }
+    void backward(float fd) {
+        forward(-fd);
+    }
+
+
+};
---
+++ CPen.h
@@ -0,0 +1,8 @@
+//
+// Created by Misha on 31.07.2026.
+//
+
+#ifndef M_CPEN_H
+#define M_CPEN_H
+
+#endif //M_CPEN_H
