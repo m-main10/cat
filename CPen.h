9e61ba880541dd233839d0715a682f024b104c5d
None <mihailburcinsij@gmail.com>
2026-08-07 09:59:16 +0300

    Update CPen.h
    CPen.h Update

--- CPen.h
+++ CPen.h
@@ -4,5 +4,34 @@
 
 #ifndef M_CPEN_H
 #define M_CPEN_H
+#include  "raylib.h"
+#include "CNaovigator.h"
+#include "Vec2d.h"
+class CPen {
+private:
+    CNavigator cn;
+    Vec2d pos;
+    Color cp;
+    float  thick;
+public:
+    void NewColor(Color color) {
+        cp = color;
 
+    }
+    CPen() {
+        cp = BLACK;
+    }
+    void NewThickness(float thick_new) {
+        thick = thick_new;
+    }
+    void forward(float fd) {
+    }
+    void backward(float fd) {
+    }
+    Color color_ret() {
+        
+
+    }
+
+};
 #endif //M_CPEN_H
